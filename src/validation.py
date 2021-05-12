b8f2f984c26b935b6e499af05f6b99b5754d811a
Alexandre do Sim <aledosim@yahoo.com.br>
2021-05-12 04:49:09 -0300

    Add validation layer with add_ad

---
+++ src/validation.py
@@ -0,0 +1,27 @@
+from schema import Schema, Regex, And
+
+add_schema = Schema({
+    'name': And(str, error='name must be str'),
+    'client': And(str, error='client must be str'),
+    'start': And(
+        And(str, error='start date must be str'),
+        Regex(r'[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}', error='start date must be in the format dd-mm-yyyy')
+    ),
+    'end': And(
+        And(str, error='end date must be str'),
+        Regex(r'[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}', error='end date must be in the format dd-mm-yyyy')
+    ),
+    'investment': And(
+        And(int, error='investment must be int'),
+        And(lambda n: n >0, error='investment must be positive'),
+    )
+})
+
+def add_ad(name=None, client=None, start=None, end=None, investment=None):
+    add_schema.validate({
+        'name': name,
+        'client': client,
+        'start': start,
+        'end': end,
+        'investment': investment,
+    })
---
+++ tests/test_validation.py
@@ -0,0 +1,107 @@
+import pytest
+
+from schema import SchemaError
+
+from src.validation import add_ad
+
+class TestAddAd:
+    # TODO
+    # test add_ad success
+
+    def test_name_must_be_str(self):
+        with pytest.raises(SchemaError) as exception:
+            add_ad(
+                name=31,
+                client='test client',
+                start='5-4-2021',
+                end='7-5-2021',
+                investment=500
+            )
+
+        assert 'name must be str' in [err for err in exception.value.errors]
+
+    def test_client_is_str(self):
+        with pytest.raises(SchemaError) as exception:
+            add_ad(
+                name='test name',
+                client=31,
+                start='5-4-2021',
+                end='7-5-2021',
+                investment=500
+            )
+
+        assert 'client must be str' in [err for err in exception.value.errors]
+
+    def test_start_is_formated_str(self):
+        with pytest.raises(SchemaError) as exception:
+            add_ad(
+                name='test name',
+                client='test client',
+                start=31,
+                end='7-5-2021',
+                investment=500
+            )
+
+        assert 'start date must be str' in [err for err in exception.value.errors]
+
+        with pytest.raises(SchemaError) as exception:
+            add_ad(
+                name='test name',
+                client='test client',
+                start='',
+                end='7-5-2021',
+                investment=500
+            )
+
+        assert 'start date must be in the format dd-mm-yyyy' in [err for err in exception.value.errors]
+
+    def test_end_is_formated_str(self):
+        with pytest.raises(SchemaError) as exception:
+            add_ad(
+                name='test name',
+                client='test client',
+                start='5-4-2021',
+                end=31,
+                investment=500
+            )
+
+        print(exception.value.autos)
+        assert 'end date must be str' in [err for err in exception.value.errors]
+
+        with pytest.raises(SchemaError) as exception:
+            add_ad(
+                name='test name',
+                client='test client',
+                start='5-4-2021',
+                end='',
+                investment=500
+            )
+
+        assert 'end date must be in the format dd-mm-yyyy' in [err for err in exception.value.errors]
+
+    def test_investment_is_positive_int(self):
+        with pytest.raises(SchemaError) as exception:
+            add_ad(
+                name='test name',
+                client='test client',
+                start='5-4-2021',
+                end='7-5-2021',
+                investment='500'
+            )
+
+        assert 'investment must be int' in [err for err in exception.value.errors]
+
+        with pytest.raises(SchemaError) as exception:
+            add_ad(
+                name='test name',
+                client='test client',
+                start='5-4-2021',
+                end='7-5-2021',
+                investment=-500
+            )
+
+        assert 'investment must be positive' in [err for err in exception.value.errors]
+
+    def test_add_ad_without_args(self):
+        with pytest.raises(SchemaError):
+            add_ad()
