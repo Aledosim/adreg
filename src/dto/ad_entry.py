f463255ea58c01c8bb1c9e0ffeb0536c3a6588c5
Alexandre do Sim <aledosim@yahoo.com.br>
2021-05-14 02:36:23 -0300

    Create AdEntryDTO

--- src/dto/ad_entry.py
+++ src/dto/ad_entry.py
@@ -1,3 +1,23 @@
+from src.dto.ad import AdDTO
 
-class AdEntryDTO:
-    pass
+class AdEntryDTO(AdDTO):
+    def __init__(self, created_at=None, updated_at=None, id=None, **kwargs):
+        super().__init__(**kwargs)
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.id = id
+
+    @staticmethod
+    def from_model(model):
+        ad_entry_dto = AdEntryDTO(
+            name=model.name,
+            client=model.client,
+            start=model.start,
+            end=model.end,
+            investment=model.investment,
+            created_at = model.created_at,
+            updated_at = model.updated_at,
+            id = model.id
+        )
+
+        return ad_entry_dto
---
+++ tests/dto/test_ad_entry_dto.py
@@ -0,0 +1,68 @@
+from datetime import date, datetime
+import pytest
+
+from src.dto.ad_entry import AdEntryDTO
+
+
+class TestAdEntry:
+    def test_init(self):
+        name = 'test name'
+        client = 'test client'
+        start = date(2021, 4, 5)
+        end = date(2021, 5, 7)
+        investment = 500
+        created_at = datetime.now()
+        updated_at = datetime.now()
+        id = 1
+
+        ad_entry_dto = AdEntryDTO(
+            name=name,
+            client=client,
+            start=start,
+            end=end,
+            investment=investment,
+            created_at=created_at,
+            updated_at=updated_at,
+            id=id
+        )
+
+        assert name == ad_entry_dto.name
+        assert client == ad_entry_dto.client
+        assert start ==  ad_entry_dto.start
+        assert end == ad_entry_dto.end
+        assert investment == ad_entry_dto.investment
+        assert created_at == ad_entry_dto.created_at
+        assert updated_at == ad_entry_dto.updated_at
+        assert id == ad_entry_dto.id
+
+    def test_from_model(self, mocker):
+        name = 'test name'
+        client = 'test client'
+        start = date(2021, 4, 5)
+        end = date(2021, 5, 7)
+        investment = 500
+        created_at = datetime.now()
+        updated_at = datetime.now()
+        id = 1
+
+        model = mocker.Mock()
+        model.name = name
+        model.client = client
+        model.start = start
+        model.end = end
+        model.investment = investment
+        model.created_at = created_at
+        model.updated_at = updated_at
+        model.id = id
+
+        ad_entry_dto = AdEntryDTO.from_model(model)
+
+        assert isinstance(ad_entry_dto, AdEntryDTO)
+        assert name == ad_entry_dto.name
+        assert client == ad_entry_dto.client
+        assert start ==  ad_entry_dto.start
+        assert end == ad_entry_dto.end
+        assert investment == ad_entry_dto.investment
+        assert created_at == ad_entry_dto.created_at
+        assert updated_at == ad_entry_dto.updated_at
+        assert id == ad_entry_dto.id
