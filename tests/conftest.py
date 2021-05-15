a68103c4cdf87a19bc14629df22f1e1bc891c5b2
Alexandre do Sim <aledosim@yahoo.com.br>
2021-05-15 01:39:16 -0300

    Refactor root conftest to set environment correctly

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,23 @@
-import pytest
 from pathlib import Path
 import tempfile
 import os
 
-def pytest_runtest_setup(item):
+
+def set_env():
     test_dir = Path(tempfile.mkdtemp())
     test_file = test_dir.joinpath('test.db')
 
     os.environ['ADREG_TEST_DB'] = str(test_file)
 
+
+def pytest_sessionstart():
+    set_env()
+
+
+def pytest_runtest_setup(item):
+    set_env()
     item.setup()
+
 
 def pytest_runtest_teardown(item):
     if 'ADREG_TEST_DB' in os.environ:
