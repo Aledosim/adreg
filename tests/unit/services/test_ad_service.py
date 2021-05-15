951c147046509eee0eb4ad83d7a1503239f9a9f2
Alexandre do Sim <aledosim@yahoo.com.br>
2021-05-15 13:58:58 -0300

    Refactored test_ad_service to use ad_input fixture

--- tests/unit/services/test_ad_service.py
+++ tests/unit/services/test_ad_service.py
@@ -7,130 +7,137 @@ import src
 from src.services.adservice import AdService
 
 
-@pytest.fixture(autouse=True)
-def ad():
-    return AdService()
-
-
 class TestAdAdd:
-    def test_passes_correct_values(self, ad, mocker):
+    def test_passes_correct_values(self, ad_input, mocker):
         mocker.patch('src.services.adservice.AdReg')
         mocker.patch('src.services.adservice.AdInputDTO')
 
-        ad.add(
-            name='test name',
-            client='test client',
+        service = AdService()
+        service.add(
+            name=ad_input.name,
+            client=ad_input.client,
             start='5-4-2021',
             end='7-5-2021',
-            investment=500
+            investment=ad_input.investment,
         )
 
         ad_dto = src.services.adservice.AdInputDTO
         ad_dto.assert_called_once_with(
-            name='test name',
-            client='test client',
+            name=ad_input.name,
+            client=ad_input.client,
             start=date(2021, 4, 5),
             end=date(2021, 5, 7),
-            investment=500
+            investment=ad_input.investment,
         )
 
         adreg = src.services.adservice.AdReg()
         adreg.create_adreg.assert_called_once_with(ad_dto())
 
-    def test_name_is_str(self, ad):
+    def test_name_is_str(self, ad_input):
+        service = AdService()
+
         with pytest.raises(SchemaError) as exception:
-            ad.add(
+            service.add(
                 name=31,
-                client='test client',
-                start='5-4-2021',
-                end='7-5-2021',
-                investment=500
+                client=ad_input.client,
+                start=ad_input.start,
+                end=ad_input.end,
+                investment=ad_input.investment,
             )
 
         assert 'name must be str' in [err for err in exception.value.errors]
 
-    def test_client_is_str(self, ad):
+    def test_client_is_str(self, ad_input):
+        service = AdService()
+
         with pytest.raises(SchemaError) as exception:
-            ad.add(
-                name='test name',
+            service.add(
+                name=ad_input.name,
                 client=31,
-                start='5-4-2021',
-                end='7-5-2021',
-                investment=500
+                start=ad_input.start,
+                end=ad_input.end,
+                investment=ad_input.investment,
             )
 
         assert 'client must be str' in [err for err in exception.value.errors]
 
-    def test_start_is_formated_str(self, ad):
+    def test_start_is_formated_str(self, ad_input):
+        service = AdService()
+
         with pytest.raises(SchemaError) as exception:
-            ad.add(
-                name='test name',
-                client='test client',
+            service.add(
+                name=ad_input.name,
+                client=ad_input.client,
                 start=31,
-                end='7-5-2021',
-                investment=500
+                end=ad_input.end,
+                investment=ad_input.investment,
             )
 
         assert 'start date must be str' in [err for err in exception.value.errors]
 
         with pytest.raises(SchemaError) as exception:
-            ad.add(
-                name='test name',
-                client='test client',
+            service.add(
+                name=ad_input.name,
+                client=ad_input.client,
                 start='',
-                end='7-5-2021',
-                investment=500
+                end=ad_input.end,
+                investment=ad_input.investment,
             )
 
         assert 'start date must be in the format dd-mm-yyyy' in [err for err in exception.value.errors]
 
-    def test_end_is_formated_str(self, ad):
+    def test_end_is_formated_str(self, ad_input):
+        service = AdService()
+
         with pytest.raises(SchemaError) as exception:
-            ad.add(
-                name='test name',
-                client='test client',
-                start='5-4-2021',
+            service.add(
+                name=ad_input.name,
+                client=ad_input.client,
+                start=ad_input.start,
                 end=31,
-                investment=500
+                investment=ad_input.investment,
             )
 
         print(exception.value.autos)
         assert 'end date must be str' in [err for err in exception.value.errors]
 
         with pytest.raises(SchemaError) as exception:
-            ad.add(
-                name='test name',
-                client='test client',
-                start='5-4-2021',
+            service.add(
+                name=ad_input.name,
+                client=ad_input.client,
+                start=ad_input.start,
                 end='',
-                investment=500
+                investment=ad_input.investment,
             )
 
         assert 'end date must be in the format dd-mm-yyyy' in [err for err in exception.value.errors]
 
-    def test_investment_is_positive_int(self, ad):
+    def test_investment_is_positive_int(self, ad_input):
+        service = AdService()
+
         with pytest.raises(SchemaError) as exception:
-            ad.add(
-                name='test name',
-                client='test client',
-                start='5-4-2021',
-                end='7-5-2021',
-                investment='500'
+            service.add(
+                name=ad_input.name,
+                client=ad_input.client,
+                start=ad_input.start,
+                end=ad_input.end,
+                investment='500',
             )
 
         assert 'investment must be int' in [err for err in exception.value.errors]
 
         with pytest.raises(SchemaError) as exception:
-            ad.add(
-                name='test name',
-                client='test client',
-                start='5-4-2021',
-                end='7-5-2021',
-                investment=-500
+            service.add(
+                name=ad_input.name,
+                client=ad_input.client,
+                start=ad_input.start,
+                end=ad_input.end,
+                investment=-500,
             )
 
         assert 'investment must be positive' in [err for err in exception.value.errors]
 
-    def test_add_without_args(self, ad):
+    def test_add_without_args(self):
+        service = AdService()
         with pytest.raises(SchemaError):
-            ad.add()
+            service.add()
