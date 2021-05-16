d6d3376f474206be170e87743ffea4afcf4066a0
Alexandre do Sim <aledosim@yahoo.com.br>
2021-05-16 12:37:37 -0300

    Massive refactor of adbusiness

--- src/business/adbusiness.py
+++ src/business/adbusiness.py
@@ -3,17 +3,84 @@ from src.dto.report import ReportDTO
 from src.libs.calculator import calculator
 
 
-def calculate_total(ad_entry):
-    n_days = ad_entry.end - ad_entry.start
-
+def calculate_total(ad_entry, delta_days):
     views, clicks, shares = calculator(ad_entry.investment)
 
-    max_views = views * n_days.days
-    max_clicks = clicks * n_days.days
-    max_shares = shares * n_days.days
-    total = ad_entry.investment * n_days.days
+    max_views = views * delta_days.days
+    max_clicks = clicks * delta_days.days
+    max_shares = shares * delta_days.days
+    total = ad_entry.investment * delta_days.days
 
     return total, max_views, max_clicks, max_shares
+
+
+def make_report_by_client(ad_entries):
+    reports = []
+    for ad_entry in ad_entries:
+        n_days = ad_entry.end - ad_entry.start
+        total, max_views, max_clicks, max_shares = calculate_total(ad_entry, n_days)
+
+        report_dto = ReportDTO.from_ad_entry(
+            ad_entry,
+            total=total,
+            max_views=max_views,
+            max_clicks=max_clicks,
+            max_shares=max_shares,
+        )
+
+        reports.append(report_dto)
+
+    return reports
+
+
+def make_report_by_period(ad_entries, report_input):
+    n_days = report_input.end - report_input.start
+    reports = []
+    for ad_entry in ad_entries:
+        total, max_views, max_clicks, max_shares = calculate_total(ad_entry, n_days)
+
+        if ad_entry.start < report_input.start:
+            ad_entry.start = report_input.start
+        if ad_entry.end > report_input.end:
+            ad_entry.end = report_input.end
+
+        report_dto = ReportDTO.from_ad_entry(
+            ad_entry,
+            total=total,
+            max_views=max_views,
+            max_clicks=max_clicks,
+            max_shares=max_shares,
+        )
+
+        reports.append(report_dto)
+
+    return reports
+
+
+def make_report_by_client_and_period(ad_entries, report_input):
+    reports = []
+    n_days = report_input.end - report_input.start
+    for ad_entry in ad_entries:
+        total, max_views, max_clicks, max_shares = calculate_total(ad_entry, n_days)
+
+        ad_entry.start = report_input.start
+        ad_entry.end = report_input.end
+
+        report_dto = ReportDTO.from_ad_entry(
+            ad_entry,
+            total=total,
+            max_views=max_views,
+            max_clicks=max_clicks,
+            max_shares=max_shares,
+        )
+
+        reports.append(report_dto)
+
+    return reports
+
+
+def make_generic_report(ad_entries):
+    return make_report_by_client(ad_entries)
 
 
 class AdReg:
@@ -25,20 +92,24 @@ class AdReg:
         return ad_entry
 
     def create_report(self, report_input_dto):
-        all_ads = self.data.find_all_ads()
 
-        reports = []
-        for ad_entry in all_ads:
-            total, max_views, max_clicks, max_shares = calculate_total(ad_entry)
 
-            report_dto = ReportDTO.from_ad_entry(
-                ad_entry,
-                total=total,
-                max_views=max_views,
-                max_clicks=max_clicks,
-                max_shares=max_shares,
-            )
+        # Filtered by client
+        if report_input_dto.client is not None and report_input_dto.start is None:
+            ads = self.data.find_ads_by_client(report_input_dto)
+            return make_report_by_client(ads)
 
-            reports.append(report_dto)
+        # Filtered by period
+        elif report_input_dto.client is None and report_input_dto.start is not None:
+            ads = self.data.find_ads_by_period(report_input_dto)
+            return make_report_by_period(ads, report_input_dto)
 
-        return reports
+        # Filtered by client and period
+        elif report_input_dto.client is not None and report_input_dto.start is not None:
+            ads = self.data.find_ads_by_client_and_period(report_input_dto)
+            return make_report_by_client_and_period(ads, report_input_dto)
+
+        # Generic report
+        else:
+            ads = self.data.find_all_ads()
+            return make_generic_report(ads)
--- tests/unit/business/test_ad_business.py
+++ tests/unit/business/test_ad_business.py
@@ -28,16 +28,18 @@ class TestAdReg:
         calculate_total_mock = mocker.patch('src.business.adbusiness.calculate_total')
         calculate_total_mock.return_value = 16000, 11200, 1344, 192,
 
-        report_input_dto.name = None
         report_input_dto.client = None
         report_input_dto.start = None
-        report_input_dto.end = None
 
         entries = [ad_entry_dto for i in range(5)]
         ad_repository_mock().find_all_ads.return_value = entries
 
         adreg = AdReg()
         result = adreg.create_report(report_input_dto)
+
+        adreg.data.find_ads_by_client.assert_not_called()
+        adreg.data.find_ads_by_period.assert_not_called()
+        adreg.data.find_ads_by_client_and_period.assert_not_called()
 
         adreg.data.find_all_ads.assert_called_once()
 
@@ -49,13 +51,63 @@ class TestAdReg:
         # Return list of ReportDTO
         assert result == [report_dto_mock.from_ad_entry() for i in range(5)]
 
+    def test_create_client_report(self, mocker, report_input_dto, ad_entry_dto):
+        ad_repository_mock = mocker.patch('src.business.adbusiness.AdRepository')
+        report_dto_mock = mocker.patch('src.business.adbusiness.ReportDTO')
+        calculate_total_mock = mocker.patch('src.business.adbusiness.calculate_total')
+        calculate_total_mock.return_value = 16000, 11200, 1344, 192,
+
+        entries = [ad_entry_dto for i in range(5)]
+        ad_repository_mock().find_ads_by_client.return_value = entries
+
+        report_input_dto.start = None
+
+        adreg = AdReg()
+        result = adreg.create_report(report_input_dto)
+
+        adreg.data.find_all_ads.assert_not_called()
+        adreg.data.find_ads_by_period.assert_not_called()
+        adreg.data.find_ads_by_client_and_period.assert_not_called()
+
+        adreg.data.find_ads_by_client.assert_called_once_with(report_input_dto)
+
+        # calculate total per entry
+        calculate_total_calls_args = [call[0][0] for call in calculate_total_mock.call_args_list]
+        for m in entries:
+            assert m in calculate_total_calls_args
+
+        # Return list of ReportDTO
+        assert result == [report_dto_mock.from_ad_entry() for i in range(5)]
+
+    def test_create_period_report(self, mocker, report_input_dto, ad_entry_dto):
+        ad_repository_mock = mocker.patch('src.business.adbusiness.AdRepository')
+        report_dto_mock = mocker.patch('src.business.adbusiness.ReportDTO')
+        calculate_total_mock = mocker.patch('src.business.adbusiness.calculate_total')
+        calculate_total_mock.return_value = 16000, 11200, 1344, 192,
+
+        entries = [ad_entry_dto for i in range(5)]
+        ad_repository_mock().find_ads_by_period.return_value = entries
+
+        report_input_dto.client = None
+
+        adreg = AdReg()
+        result = adreg.create_report(report_input_dto)
+
+        adreg.data.find_all_ads.assert_not_called()
+        adreg.data.find_ads_by_client.assert_not_called()
+        adreg.data.find_ads_by_client_and_period.assert_not_called()
+
+        adreg.data.find_ads_by_period.assert_called_once_with(report_input_dto)
+
 
 class TestCalculateTotal:
     def test_calculate_total(self, ad_entry_dto, mocker):
         calculator_mock = mocker.patch('src.business.adbusiness.calculator')
         calculator_mock.return_value = 350, 42, 6,
 
-        result = calculate_total(ad_entry_dto)
+        n_days = ad_entry_dto.end - ad_entry_dto.start
+
+        result = calculate_total(ad_entry_dto, n_days)
 
         calculator_mock.assert_called_with(ad_entry_dto.investment)
 
