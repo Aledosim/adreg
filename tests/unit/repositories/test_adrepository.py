88e174fa7024af8e001bf046cb1e88f350201545
Alexandre do Sim <aledosim@yahoo.com.br>
2021-05-16 12:33:52 -0300

    Create find_ads_by_client, find_ads_by_period, find_ads_by_client_and_period methods of AdRepository

--- tests/unit/repositories/test_adrepository.py
+++ tests/unit/repositories/test_adrepository.py
@@ -40,7 +40,7 @@ class TestData:
         data = AdRepository()
         result = data.find_all_ads.__wrapped__(data)
 
-        ad_class.select.assert_called_once_with()
+        ad_class.select.assert_called_once()
 
         # Map Ad instances to AdEntryDTO
         from_model_calls_args = [call[0][0] for call in ad_entry_class.from_model.call_args_list]
@@ -49,3 +49,32 @@ class TestData:
 
         # Return AdEntryDTO list
         assert result == [ad_entry_class.from_model() for i in range(5)]
+
+    def test_find_ads_by_client(self, mocker, report_input_dto):
+        ad_entry_class = mocker.patch('src.repositories.adrepository.AdEntryDTO')
+        ad_class = mocker.patch('src.repositories.adrepository.Ad')
+
+        query_result = [mocker.Mock() for i in range(5)]
+        ad_class.select.return_value.where.return_value = query_result
+
+        data = AdRepository()
+        result = data.find_ads_by_client.__wrapped__(data, report_input_dto)
+
+        ad_class.select().where.assert_called_once()
+
+        # Map Ad instances to AdEntryDTO
+        from_model_calls_args = [call[0][0] for call in ad_entry_class.from_model.call_args_list]
+        for m in query_result:
+            assert m in from_model_calls_args
+
+        # Return AdEntryDTO list
+        assert result == [ad_entry_class.from_model() for i in range(5)]
+
+    def test_find_ads_by_period(self, mocker, report_input_dto):
+        select_spy = mocker.spy(src.repositories.adrepository.Ad, 'select')
+        ad_entry_class = mocker.patch('src.repositories.adrepository.AdEntryDTO')
+
+        data = AdRepository()
+        result = data.find_ads_by_period.__wrapped__(data, report_input_dto)
+
+        select_spy.assert_called_once()
