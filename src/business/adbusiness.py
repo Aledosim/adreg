3c61eca40a6e6721525566c7a1aa5f084eb33953
Alexandre do Sim <aledosim@yahoo.com.br>
2021-05-16 01:47:32 -0300

    Create create_report method of AdReg and calculate_total function

--- src/business/adbusiness.py
+++ src/business/adbusiness.py
@@ -1,4 +1,19 @@
 from src.repositories.adrepository import AdRepository
+from src.dto.report import ReportDTO
+from src.libs.calculator import calculator
+
+
+def calculate_total(ad_entry):
+    n_days = ad_entry.end - ad_entry.start
+
+    views, clicks, shares = calculator(ad_entry.investment)
+
+    max_views = views * n_days.days
+    max_clicks = clicks * n_days.days
+    max_shares = shares * n_days.days
+    total = ad_entry.investment * n_days.days
+
+    return total, max_views, max_clicks, max_shares
 
 
 class AdReg:
@@ -10,4 +25,20 @@ class AdReg:
         return ad_entry
 
     def create_report(self, report_input_dto):
-        pass
+        all_ads = self.data.find_all_ads()
+
+        reports = []
+        for ad_entry in all_ads:
+            total, max_views, max_clicks, max_shares = calculate_total(ad_entry)
+
+            report_dto = ReportDTO.from_ad_entry(
+                ad_entry,
+                total=total,
+                max_views=max_views,
+                max_clicks=max_clicks,
+                max_shares=max_shares,
+            )
+
+            reports.append(report_dto)
+
+        return reports
--- tests/unit/business/test_ad_business.py
+++ tests/unit/business/test_ad_business.py
@@ -1,6 +1,7 @@
+import pytest
 import src
 
-from src.business.adbusiness import AdReg
+from src.business.adbusiness import AdReg, calculate_total
 
 
 class TestAdReg:
@@ -21,9 +22,41 @@ class TestAdReg:
         adreg.data.get_or_create_ad.assert_called_once_with(ad_input_dto)
         assert adreg.data.get_or_create_ad() == result
 
-    def test_create_report(self, mocker, report_input_dto):
-        mocker.patch('src.business.adbusiness.AdRepository')
+    def test_create_generic_report(self, mocker, report_input_dto, ad_entry_dto):
+        ad_repository_mock = mocker.patch('src.business.adbusiness.AdRepository')
+        report_dto_mock = mocker.patch('src.business.adbusiness.ReportDTO')
+        calculate_total_mock = mocker.patch('src.business.adbusiness.calculate_total')
+        calculate_total_mock.return_value = 16000, 11200, 1344, 192,
+
+        report_input_dto.name = None
+        report_input_dto.client = None
+        report_input_dto.start = None
+        report_input_dto.end = None
+
+        entries = [ad_entry_dto for i in range(5)]
+        ad_repository_mock().find_all_ads.return_value = entries
 
         adreg = AdReg()
         result = adreg.create_report(report_input_dto)
 
+        adreg.data.find_all_ads.assert_called_once()
+
+        # calculate total per entry
+        calculate_total_calls_args = [call[0][0] for call in calculate_total_mock.call_args_list]
+        for m in entries:
+            assert m in calculate_total_calls_args
+
+        # Return list of ReportDTO
+        assert result == [report_dto_mock.from_ad_entry() for i in range(5)]
+
+
+class TestCalculateTotal:
+    def test_calculate_total(self, ad_entry_dto, mocker):
+        calculator_mock = mocker.patch('src.business.adbusiness.calculator')
+        calculator_mock.return_value = 350, 42, 6,
+
+        result = calculate_total(ad_entry_dto)
+
+        calculator_mock.assert_called_with(ad_entry_dto.investment)
+
+        assert (16000, 11200, 1344, 192) == result
